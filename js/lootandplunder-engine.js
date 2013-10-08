6ebf266681e49008ac7f8dffebd378e246145e81
Chris Mulder <chris.m@Live.nl>
2013-10-08 19:08:39 +0200

    Added gravity and jumping.

--- js/lootandplunder-engine.js
+++ js/lootandplunder-engine.js
@@ -22,7 +22,7 @@ var animations = {
 					height: 104
 				}
 			],
-			moving-right: [
+			moving_right: [
 				{
 					x: 54,
 					y: 58,
@@ -73,7 +73,7 @@ var animations = {
 					height: 99
 				}
 			],
-			moving-left: [
+			moving_left: [
 				{
 					x: 54 + 1188,
 					y: 58,
@@ -159,6 +159,8 @@ var blocks = {
 		}
 	}
 
+var gravity = -9;
+
 var block_sprites = new Image();
 block_sprites.src = "resource/img/blocks.png";
 
@@ -166,6 +168,12 @@ function Character(x, y, animation, state) {
 	var _currentRefresh = 0; // higher is les
 	var _x = x;
 	var _y = y;
+	
+	var _dx = 0;
+	var _dy = 0;
+	
+	var _jump = false;
+	
 	var _animation = animation;
 	var _state = state;
 	
@@ -183,6 +191,14 @@ function Character(x, y, animation, state) {
 		_state = state;
 	}
 	
+	this.isJumping = function() {
+		return _jump;
+	}
+	
+	this.jump = function() {
+		_jump = true;
+	}
+	
 	this.getAnimation = function() {
 		return _animation;
 	}
@@ -195,8 +211,43 @@ function Character(x, y, animation, state) {
 		return _animation.speed;
 	}
 	
+	this.collideWithGround = function() {
+		var frame = _state[_current_frame];
+		for(var i = 0; i < layer.background.length; i++) {
+			var block = layer.background[i];
+			if(_y + frame.height >= block.getY()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	this.update = function() {
+		if(_current_frame >= _state.length - 1) {
+			_current_frame = 0;
+		}
+		
 		_currentRefresh++;	
+		
+		if(this.collideWithGround()) { 
+			if(_jump) {
+				_dy = -10;
+			} else {
+				_dy = 0;
+			}
+		} else {
+			_dy -= -0.5;
+		}
+		
+		if(_dy > 5)
+			_dy = 5;
+		
+		this.move(_dx, _dy);
+
+		if(_jump) {
+			_jump = false;
+		}
+		
 		// limit refreshing
 		if((_currentRefresh % _animation.refreshRate) === 0){
 			if(_current_frame >= _state.length - 1) {
@@ -208,9 +259,7 @@ function Character(x, y, animation, state) {
 	}
 	
 	this.draw = function(dx) {
-		if(_current_frame >= _state.length) {
-			_current_frame = 0;
-		}
+
 		var frame = _state[_current_frame];
 		
 		dx.drawImage(_img, frame.x, frame.y, frame.width, frame.height, _x, _y, frame.width, frame.height);
@@ -229,16 +278,23 @@ function Block(x, y, img_coords) {
 		//Check for colission here or what
 	}
 	
+	this.getX = function() {
+		return _x;
+	}
+	this.getY = function() {
+		return _y;
+	}
+	
 	this.draw = function(dx) {
 		dx.drawImage(block_sprites, _img_x, _img_y, _width, _height, _x, _y, _width, _height);
 	}
 }
 
 var keys = []
-var player = new Character(20, 20, animations.player, animations.player.moving);
-var player2 = new Character(50, 50, animations.player, animations.player.idle);
+var player = new Character(40, 400, animations.player, animations.player.moving);
+
 var layer = {
-		background: [new Block(10, 10, blocks.grass_left), new Block(42, 10, blocks.grass_mid)],
+		background: [],
 		loot: [],
 		characters: [ player]
 }
@@ -273,7 +329,16 @@ $(function() {
 	});
 	
 	
+	function generate_ground() {
+		layer.background.push(new Block(0, 600-32, blocks.grass_left));
+		for(var i = 32; i < (800 -32); i += 32) {
+			layer.background.push(new Block(i, 600-32, blocks.grass_mid));
+		}
+		layer.background.push(new Block(800-32, 600-32, blocks.grass_right));
+	}
+	
 	function initialize() {
+		generate_ground();
 	}
 	
 
@@ -293,14 +358,20 @@ $(function() {
 		if(keys[37]) {
 			moveX -= player.getSpeed();
 		}
+		if(keys[32]) {
+			if(!player.isJumping()) {
+				player.jump();
+			}
+		}
+		
 		if(moveX === 0) {
 			player.setState(player.getAnimation().idle);
 		} else {
 			player.move(moveX, 0);
 			if(moveX < 0){
-				player.setState(player.getAnimation().moving-left);
+				player.setState(player.getAnimation().moving_left);
 			}else{
-				player.setState(player.getAnimation().moving-right);
+				player.setState(player.getAnimation().moving_right);
 			}
 		}
 		
